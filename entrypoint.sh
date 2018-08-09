14b7994a11e258423b6152097c1d367798ec808f
Roderik van der Veer <roderik@settlemint.com>
2018-08-09 12:59:45 +0200

    fix: move to the new mintnet settings

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,16 @@
 
 JSONRPC_PORT=${JSONRPC_PORT:-'8545'}
 WEBSOCKETS_PORT=${WEBSOCKETS_PORT:-'8546'}
-NETWORK_PORT=${NETWORK_PORT:-'30303'}
+NETWORK_PORT=${NETWORK_PORT:-'30311'}
 
 /parity/parity --config /etc/mintnet/mintnet.toml \
     --jsonrpc-port=$JSONRPC_PORT \
     --jsonrpc-cors all \
-    --jsonrpc-threads=8 \
+    --ws-origins all \
     --port=$NETWORK_PORT \
     --ws-port=$WEBSOCKETS_PORT \
     --unsafe-expose \
-    --reserved-only
+    --min-gas-price=0 \
+    --tx-queue-gas=off \
+    --tx-queue-mem-limit=0 \
+    --tx-queue-size=131072
