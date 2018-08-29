b2952a96aa83d95f1820f20164cb9d387e3b0365
Roderik van der Veer <roderik@settlemint.com>
2018-08-29 09:07:51 +0200

    fix: reset settings to pre upgrade

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,4 @@ NETWORK_PORT=${NETWORK_PORT:-'30311'}
     --ws-origins all \
     --port=$NETWORK_PORT \
     --ws-port=$WEBSOCKETS_PORT \
-    --unsafe-expose \
-    --min-gas-price=0 \
-    --tx-queue-mem-limit=0 \
-    --tx-queue-size=131072
+    --unsafe-expose
