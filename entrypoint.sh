fab4397c0e480b4fbc44fde1c6ee964eda9ce316
Roderik van der Veer <roderik@settlemint.com>
2018-11-25 19:13:58 +0400

    fix: new path

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ JSONRPC_PORT=${JSONRPC_PORT:-'8545'}
 WEBSOCKETS_PORT=${WEBSOCKETS_PORT:-'8546'}
 NETWORK_PORT=${NETWORK_PORT:-'30311'}
 
-/parity/parity --config /etc/mintnet/mintnet.toml \
+/home/parity/bin/parity --config /etc/mintnet/mintnet.toml \
     --jsonrpc-port=$JSONRPC_PORT \
     --port=$NETWORK_PORT \
     --ws-port=$WEBSOCKETS_PORT \
